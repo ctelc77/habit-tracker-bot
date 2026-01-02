1f6948d14943e175c57cd363ff9087879d6c9085
ctelc77 <122752895+ctelc77@users.noreply.github.com>
2026-01-02 14:01:59 +0200

    Update supabaseClient.js

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
 // Ці дані візьми в налаштуваннях Supabase (Project Settings -> API)
-const supabaseUrl = 'ТВІЙ_URL_З_SUPABASE';
-const supabaseAnonKey = 'ТВІЙ_ANON_KEY_З_SUPABASE';
+const supabaseUrl = 'https://dfhlppkkfbjqtosqdjpq.supabase.co';
+const supabaseAnonKey = 'sb_publishable_XRr2MyH6KYisWEOdX-VDqQ_-9Ah2uRn';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
